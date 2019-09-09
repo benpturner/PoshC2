5c30560f9f725b9d531fd397a5c6483c4730b5b3
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2019-09-09 12:51:44 +0100

    Work on install_and_setup

--- conf.py
+++ conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PoshC2 Framework'
-copyright = u'2018, Nettitude'
+copyright = u'2019, Nettitude'
 author = u'Ben Turner'
 
 # The version info for the project you're documenting, acts as replacement for
